7f75b8b64fa5faabe61e872230376a1d319cddb8
Christopher Dorst <chris@dorst.email>
2018-01-12 12:34:22 -0600

    Add JsonApiRequestSender

---
+++ Common.Functions.SendJsonApiRequest/JsonApiRequestSender.cs
@@ -0,0 +1,23 @@
+﻿using Common.Structures.HttpAuthentication;
+using Common.Structures.HttpJsonRequest;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Common.Functions.SendJsonApiRequest
+{
+    public static class JsonApiRequestSender
+    {
+        public static async Task<HttpResponseMessage> ApiRequest(ApiRequest request, HttpAuthentication authentication = null)
+        {
+            if (request == null) throw new ArgumentNullException(nameof(request));
+            using (var client = new HttpClient { BaseAddress = request.BaseAddress })
+            {
+                if (authentication != null)
+                    client.DefaultRequestHeaders.Authorization = authentication.Header;
+
+                return await client.SendAsync(request.Message);
+            }
+        }
+    }
+}
